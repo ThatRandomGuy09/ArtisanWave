f46704fdad4be5ac33a70af95b06a699203abdd5
Vansh Vasishtha <vansh19092002@gmail.com>
2024-02-08 13:54:19 +0530

    Moving functionality Added

--- app/page.tsx
+++ app/page.tsx
@@ -10,10 +10,13 @@ import { useEffect, useRef, useState } from "react";
 import { init } from "next/dist/compiled/webpack/webpack";
 import {
   handleCanvasMouseDown,
+  handleCanvaseMouseMove,
+  handleCanvasMouseUp,
   handleResize,
   initializeFabric,
 } from "@/lib/canvas";
 import { ActiveElement } from "@/types/type";
+import { useMutation, useStorage } from "@/liveblocks.config";
 
 export default function Page() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -22,21 +25,32 @@ export default function Page() {
   const shapeRef = useRef<fabric.Object | null>(null);
   const selectedShapeRef = useRef<string | null>("rectangle");
 
+  const canvasObjects = useStorage((root) => root.canvasObjects);
+  const activeObjectRef = useRef<fabric.Object | null>(null);
+
+  const syncShapeInStorage = useMutation(({ storage }, object) => {
+    if (!object) return;
+
+    const { objectId } = object;
+
+    const shapeData = object.toJSON();
+    shapeData.objectId = objectId;
+
+    const canvasObjects = storage.get("canvasObjects");
+    canvasObjects.set(objectId, shapeData);
+  }, []);
 
   const [activeElement, setActiveElement] = useState<ActiveElement>({
-
-    name:'',
-    value:'',
-    icon:''
-
-
-  })
+    name: "",
+    value: "",
+    icon: "",
+  });
 
   const handleActiveElement = (elem: ActiveElement) => {
     setActiveElement(elem);
 
     selectedShapeRef.current = elem?.value as string;
-  }
+  };
 
   useEffect(() => {
     const canvas = initializeFabric({
@@ -54,6 +68,30 @@ export default function Page() {
       });
     });
 
+    canvas.on("mouse:move", (options) => {
+      handleCanvaseMouseMove({
+        options,
+        canvas,
+        isDrawing,
+        shapeRef,
+        selectedShapeRef,
+        syncShapeInStorage,
+      });
+    });
+
+    canvas.on("mouse:up", (options) => {
+      handleCanvasMouseUp({
+        canvas,
+        isDrawing,
+        shapeRef,
+        selectedShapeRef,
+        syncShapeInStorage,
+        setActiveElement,
+        activeObjectRef
+
+      });
+    });
+
     window.addEventListener("resize", () => {
       handleResize({ fabricRef });
     });
@@ -62,8 +100,8 @@ export default function Page() {
   return (
     <main className="h-screen overflow-hidden">
       <NavBar
-      activeElement={activeElement}
-      handleActiveElement={handleActiveElement}
+        activeElement={activeElement}
+        handleActiveElement={handleActiveElement}
       />
       <section className="flex h-full flex-row">
         <LeftSidebar />
