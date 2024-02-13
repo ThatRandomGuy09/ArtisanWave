c59c799c435fa1105ef9313fd527c97164ad80d0
Vansh Vasishtha <vansh19092002@gmail.com>
2024-02-13 19:11:50 +0530

    Comments Feature added from liveblocks

--- components/comments/CommentsOverlay.tsx
+++ components/comments/CommentsOverlay.tsx
@@ -3,7 +3,12 @@
 import { useCallback, useRef } from "react";
 import { ThreadData } from "@liveblocks/client";
 
-import { ThreadMetadata, useEditThreadMetadata, useThreads, useUser } from "@/liveblocks.config";
+import {
+  ThreadMetadata,
+  useEditThreadMetadata,
+  useThreads,
+  useUser,
+} from "@/liveblocks.config";
 import { useMaxZIndex } from "@/lib/useMaxZIndex";
 
 import { PinnedThread } from "./PinnedThread";
@@ -14,15 +19,8 @@ type OverlayThreadProps = {
 };
 
 export const CommentsOverlay = () => {
-  /**
-   * We're using the useThreads hook to get the list of threads
-   * in the room.
-   *
-   * useThreads: https://liveblocks.io/docs/api-reference/liveblocks-react#useThreads
-   */
   const { threads } = useThreads();
 
-  // get the max z-index of a thread
   const maxZIndex = useMaxZIndex();
 
   return (
@@ -30,38 +28,28 @@ export const CommentsOverlay = () => {
       {threads
         .filter((thread) => !thread.metadata.resolved)
         .map((thread) => (
-          <OverlayThread key={thread.id} thread={thread} maxZIndex={maxZIndex} />
+          <OverlayThread
+            key={thread.id}
+            thread={thread}
+            maxZIndex={maxZIndex}
+          />
         ))}
     </div>
   );
 };
 
 const OverlayThread = ({ thread, maxZIndex }: OverlayThreadProps) => {
-  /**
-   * We're using the useEditThreadMetadata hook to edit the metadata
-   * of a thread.
-   *
-   * useEditThreadMetadata: https://liveblocks.io/docs/api-reference/liveblocks-react#useEditThreadMetadata
-   */
   const editThreadMetadata = useEditThreadMetadata();
 
-  /**
-   * We're using the useUser hook to get the user of the thread.
-   *
-   * useUser: https://liveblocks.io/docs/api-reference/liveblocks-react#useUser
-   */
   const { isLoading } = useUser(thread.comments[0].userId);
 
-  // We're using a ref to get the thread element to position it
   const threadRef = useRef<HTMLDivElement>(null);
 
-  // If other thread(s) above, increase z-index on last element updated
   const handleIncreaseZIndex = useCallback(() => {
     if (maxZIndex === thread.metadata.zIndex) {
       return;
     }
 
-    // Update the z-index of the thread in the room
     editThreadMetadata({
       threadId: thread.id,
       metadata: {
